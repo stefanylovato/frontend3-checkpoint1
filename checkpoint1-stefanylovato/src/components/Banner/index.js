62678f47c027f67be42d69bfa8c5287bd7506d3c
Stefany Lovato <stefanylovato@gmail.com>
2021-11-19 13:27:08 -0300

    all project's requests are done

--- checkpoint1-stefanylovato/src/components/Banner/index.js
+++ checkpoint1-stefanylovato/src/components/Banner/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import profilePicture from '../../assets/profile-picture.png'
 import './style.scss';
 
 const Banner = () => {
@@ -6,12 +8,12 @@ const Banner = () => {
         <section id="banner">
             <div className="banner-text">
                 <div className="row featurette">
-                    <div class="col-md-6">
-                        <h2 className="display-4">Hey there!<span style={{color: 'var(--blue)'}}> I'm Stefany Lovato</span></h2>
-                        <p className="lead">I'm a coder and designer who love to build beautiful things</p>
+                    <div class="col-md-6 ">
+                        <h2 className="display-4 mt-5 " style={{color: 'var(--blue)'}}>Hey there!<span> I'm Stefany Lovato</span></h2>
+                        <p className="lead mt-4">I'm a coder and designer who love build beautiful and functional things</p>
                     </div>
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                    <div className="col-md-6 ">
+                        <img src={profilePicture} alt="photo of me" className='img-fluid'/>
 
                     </div>
                 </div>
